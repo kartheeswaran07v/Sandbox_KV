baabece2d86ebb8b1452f97c71d4a58ff00d80fe
FCC <kartheeswaran07v@gmail.com>
2023-06-06 20:35:46 +0530

    db.create_all() commented

--- main.py
+++ main.py
@@ -79,8 +79,8 @@ class Upload(db.Model):
     data = Column(LargeBinary)
 
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 
 @app.route('/', methods=["GET", "POST"])
